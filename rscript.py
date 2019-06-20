4d45147537a09b064634d46a5cdc98c07f457705
Nikhil <nikhil2@gmail.com>
2019-06-20 16:59:02 -0400

    d

--- rscript.py
+++ rscript.py
@@ -15,18 +15,29 @@ with open('C:\Users\NGopal1\Desktop', 'wb') as f:
     f.close()
 '''
 
-pdfFileObj = open("C:\Users\NGopal1\Desktop\scriptname.pdf", 'rb') #read the pdf
+'''pdfFileObj = open("C:\Users\NGopal1\Desktop\scriptname.pdf", 'rb') #read the pdf
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 number_of_pages = pdfReader.numPages
 whole_pdf_document = [] #initialize a list to hold the pages of the pdf
+pageObj = pdfReader.getPage(0)
+'''
 
-for line in range(0,number_of_pages): #iterate through the pdf document, add each page to its own index in the list
-    pageObj = pdfReader.getPage(line)
-    whole_pdf_document.append(pageObj.extractText())
+temp_file_handle = open("tempfile.txt", "r")
+
+'''for line in range(0,number_of_pages): #iterate through the pdf document, add each page to its own index in the list
+     pageObj = pdfReader.getPage(line)
+     whole_pdf_document.append(pageObj.extractText())
+     temp_file_handle.write(whole_pdf_document[line])
+     temp_file_handle.write("*****page break******")
+'''
 
 
-for page_number in range(0,len(whole_pdf_document)):
+whole_pdf_line_by_line = temp_file_handle.readlines()
+
+for index in whole_pdf_line_by_line:
+
+
 
 
 
