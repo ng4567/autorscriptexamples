609ee396608d6381747461cc8784dfaced881414
Nikhil Gopal <nikhil2@gmail.com>
2019-08-13 12:41:58 -0400

    Works now

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-import wget
+import wget #import necessary libraries
 import PyPDF2
 import os
+
+abspath = os.path.abspath(__file__) #change working  directory to where the script is, so that document is written there
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 url= raw_input("Please enter the url to the package documentation: ")
 wget.download(url, "file.pdf")
@@ -14,8 +18,7 @@ whole_pdf_document = []  # initialize a list to hold the pages of the pdf
 
 path_to_text_file = "holdingfile.txt"
 
-temp_file_handle = open(path_to_text_file, "w")
-
+temp_file_handle = open(path_to_text_file, "w") 
 
 for line in range(0,number_of_pages): #iterate through the pdf document, add each page to its own index in the list
      pageObj = pdfReader.getPage(line)
@@ -83,12 +86,92 @@ def parsefiles():
         file_with_example_code_file_handle.write("\n")
         file_with_example_code_file_handle.write(line)
 
+
      file_with_example_code_file_handle.close()  #close open files
-     os.remove(path_to_pdf) #delete files not needed anymore
      os.remove(path_to_text_file) #delete files not needed anymore
      print "Done"
 
-parsefiles()
+     return True
+
+parsefiles() #call function to extract example code (still reads apostrophes as new lines)
+
+list_of_code =[]
+with open("examples.txt", "rt") as code: #get the code into an iterabole format (list)
+    for line in code:
+        list_of_code.append(line)
+
+apostrophe_lines = []
+lines_to_not_write = []
+index_counter = 0
+for index in list_of_code: #figure out where the lines are that have apostrophes
+    if index == "'\n" or index == "`\n":
+        apostrophe_lines.append(index_counter)
+        lines_to_not_write.append(index_counter-1)
+        lines_to_not_write.append(index_counter)
+        lines_to_not_write.append(index_counter+1)
+    index_counter += 1
+
+fh=open("test.txt","w")
+
+line_number = 0
+for line in list_of_code:  #merge the lines with apostrophes with the lines before and after, undoing the bad PDF scraping
+    if line_number in lines_to_not_write:
+        if line_number in apostrophe_lines:
+            a= list_of_code[line_number-1]
+            b=list_of_code[line_number]
+            c=list_of_code[line_number+1]
+            a=a.rstrip("\n")
+            b=b.rstrip("\n")
+            c=c.rstrip("\n")
+            fh.write(a+b+c+"\n")
+    else:
+        fh.write(line)
+    line_number += 1
+
+fh.close()
+
+list_of_code =[]
+with open("test.txt", "rt") as code:
+    for line in code:
+        list_of_code.append(line)
 
 
+apostrophe_lines = []
+lines_to_not_write = []
+index_counter = 0
+for index in list_of_code: #do the same process again in case there are apostrophes next to each other
+    if index == "'\n" or index == "`\n":
+        apostrophe_lines.append(index_counter)
+        lines_to_not_write.append(index_counter-1)
+        lines_to_not_write.append(index_counter)
+        lines_to_not_write.append(index_counter+1)
+    index_counter += 1
+
+
+
+
+fh=open("CodeExamples.txt","w")
+
+line_number = 0
+for line in list_of_code:
+    if line_number in lines_to_not_write:
+        if line_number in apostrophe_lines:
+            a= list_of_code[line_number-1]
+            b=list_of_code[line_number]
+            c=list_of_code[line_number+1]
+            a=a.rstrip("\n")
+            b=b.rstrip("\n")
+            c=c.rstrip("\n")
+            fh.write(a+b+c+"\n")
+    else:
+        fh.write(line)
+    line_number += 1
+
+fh.close()
+
+
+
+os.remove("test.txt") #delete unnecessary files
+os.remove("examples.txt")
+os.remove("file.pdf")
 
